4272e4e42197ab39106e549d4e07743c6b2deb5c
Logan Tran <loganmartintran@icloud.com>
2016-12-27 11:25:49 -0600

    Refactor style calls

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,13 @@
 var animatePoints = function () {
     var points = document.getElementsByClassName("point");
-    var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    var revealSecondPoint = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transfrom = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    var revealThirdPoint = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    function revealPoint (index) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
     
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-    
+    for (var i=0; i<points.length; i++) {
+        revealPoint(i);
+    }
 };
