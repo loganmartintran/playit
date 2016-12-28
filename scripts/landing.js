cbee479614e65c013bdf9bad11cd09eafd86caa0
Logan Tran <loganmartintran@icloud.com>
2016-12-27 22:32:28 -0600

    Completed checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function () {
-    var points = document.getElementsByClassName("point");
-    function revealPoint (index) {
+var pointsArray = document.getElementsByClassName('point');
+
+function animatePoints(points) {
+    function revealPoint(index) {
         points[i].style.opacity = 1;
         points[i].style.transform = "scaleX(1) translateY(0)";
         points[i].style.msTransform = "scaleX(1) translateY(0)";
@@ -11,3 +12,18 @@ var animatePoints = function () {
         revealPoint(i);
     }
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
