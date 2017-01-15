8d5738cd1b8f6b9c42dc69667001576c68036b6f
Logan Tran <loganmartintran@icloud.com>
2017-01-14 19:47:58 -0600

    modified several syntax issues

--- scripts/album.js
+++ scripts/album.js
@@ -47,7 +47,7 @@ var albumSweetTunes = {
 var createSongRow = function (songNumber, songName, songLength) {
     var template =  
         '<tr class="album-view-song-item">'
-     +  '<td class="song-item-number" "data-song-number"=' + songNumber + '">' + songNumber + '</td>'
+     +  '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      +  '  <td class="song-item-title">' + songName + '</td>'
      +  '  <td class="song-item-duration">' + songLength + '</td>'
      +  '</tr>'
