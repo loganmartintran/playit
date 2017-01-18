63aa4ea040862ba40ca6b7f2bdab755322b236ea
Logan Tran <loganmartintran@icloud.com>
2017-01-17 22:19:09 -0600

    completed checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,25 @@
-var pointsArray = document.getElementsByClassName('point');
-
-function animatePoints(pointsArray) {
-    function revealPoint(i) {
-        pointsArray[i].style.opacity = 1;
-        pointsArray[i].style.transform = "scaleX(1) translateY(0)";
-        pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
-        pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
     
-    forEach(pointsArray, revealPoint);
+    $.each($('.point'), revealPoint);
     
 };
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
     }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
+});
